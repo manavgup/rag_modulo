ca04b4b1d72cba6d617cd46a9c93786af5d32d10
Maksym Tykhenko <maksym.tykhenko@gmail.com>
2024-11-26 23:13:52 -0500

    Make files storage folder configurable for backend (currently only temp folder is an option)

--- backend/core/config.py
+++ backend/core/config.py
@@ -48,7 +48,7 @@ class Settings(BaseSettings):
     log_level: Optional[str] = None
 
     # File storage path
-    file_storage_path: str = tempfile.gettempdir()
+    file_storage_path: str = Field(default=tempfile.gettempdir(), env='FILE_STORAGE_PATH')
 
     # ChromaDB credentials
     chromadb_host: Optional[str] = None
