d4d956e750714d81180874a64d54d2364d588c6e
JS Lecointre <jslecointre@JSs-MacBook-Pro.local>
2024-12-13 13:41:30 -0500

    fix(chunk_id): set chunk_id back to uuid.uuid4()

--- backend/rag_solution/data_ingestion/pdf_processor.py
+++ backend/rag_solution/data_ingestion/pdf_processor.py
@@ -138,7 +138,7 @@ class PdfProcessor(BaseProcessor):
 
     def create_document_chunk(self, chunk_text: str, chunk_embedding: List[float], metadata: Dict[str, Any],
                               document_id: str) -> DocumentChunk:
-        chunk_id = hashlib.md5(chunk_text.encode('utf-8')).hexdigest()
+        chunk_id = str(uuid.uuid4())
         return DocumentChunk(
             chunk_id=chunk_id,
             text=chunk_text,
